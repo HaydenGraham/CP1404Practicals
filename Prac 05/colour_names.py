48f493a3f9622543fccfb5c0ad996d10b6ce84e9
jc431215 <hayden.graham@my.jcu.edu.au>
2017-04-03 11:10:13 +1000

    colour nameing to the program

---
+++ Prac 05/colour_names.py
@@ -0,0 +1,17 @@
+
+HEX_NAMES = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7", "azure1": "#f0ffff", "blueviolet": "#8a2be2", "blue1": "#0000ff", "cadetblue": "#5f9ea0"}
+
+
+color = (input("Enter colour name: ")).lower()
+
+while color != "":
+
+    if color in HEX_NAMES:
+
+        print(color, "is", HEX_NAMES[color])
+
+    else:
+
+        print("Invalid colour meme")
+
+    color = (input("Enter colour name: ")).lower()
