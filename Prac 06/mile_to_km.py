bf1427468034134c3443c257b20d3c709c8779c1
jc431215 <g>
2017-04-10 12:38:16 +1000

    Completed

--- Prac 06/mile_to_km.py
+++ Prac 06/mile_to_km.py
@@ -8,7 +8,7 @@ class mile_to_km(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
+        Window.size = (300, 500)
         self.title = "Square Number"
         self.root = Builder.load_file('mile_to_km.kv')
         return self.root
@@ -16,11 +16,20 @@ class mile_to_km(App):
         result = value + 1
         self.root.ids.input_number.text = str(result)
     def takeone(self, value):
-        result = value - 1
+        try:
+            result = value - 1
+
+        except ValueError:
+            print("thisran")
+            self.root.ids.input_number.text = 0
+
         self.root.ids.input_number.text = str(result)
     def handle_calculate(self, value):
-        print(value/1.6)
-        result = value/1.6
-        self.root.ids.output_label.text = str(result)
+        try:
+            print(value/1.6)
+            result = value/1.6
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            self.root.ids.output_label.text = 0.0
 
 mile_to_km().run()
